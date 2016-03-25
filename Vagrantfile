cb98aa4e323660706fb8159361c151769abfae51
Oleg Tokarev <otokarev@openprovider.ru>
2016-03-25 16:30:09 +0600

    Initial Commit

---
+++ Vagrantfile
@@ -0,0 +1,70 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "centos/7"
+
+  config.vm.provision "shell" do |s|
+    ssh_pub_key = File.readlines("#{Dir.home}/.ssh/id_rsa.pub").first.strip
+    s.inline = <<-SHELL
+      echo #{ssh_pub_key} >> /home/vagrant/.ssh/authorized_keys
+      chmod 600 /home/vagrant/.ssh/authorized_keys
+    SHELL
+  end
+
+#  config.vm.define "docker-registry1" do |c|
+#    c.vm.network "private_network", ip: "192.168.2.4"
+#    c.vm.hostname = "hui.boom"
+#    c.vm.provision "ansible" do |a|
+#      a.limit = "docker-registry1"
+#      a.playbook = "playbooks/docker-registry/playbook.yml"
+#      a.raw_arguments = [
+#        '-K'
+#      ]
+#    end
+#  end
+
+    (2..4).each do |i|
+      config.vm.define "master#{i}" do |c|
+        c.vm.provider "virtualbox" do |v|
+          v.cpus = 2
+        end
+        c.vm.network "private_network", ip: "192.168.2.#{i}"
+        c.vm.hostname = "master#{i}.mesos.boom"
+#        c.vm.provision "ansible" do |a|
+#          a.limit = "all"
+#          a.playbook = "playbooks/mesos-single/playbook.yml"
+#          a.groups = {
+#            "mesos-single" => ["master#{i}"]
+#          }
+#          a.extra_vars = {
+#            domain_name: "master#{i}.mesos.boom",
+#            host_ip: "192.168.2.#{i}",
+#            zoo_myid: "#{i}"  
+#            }
+#        end
+      end
+    end
+
+    (5..7).each do |i|
+      config.vm.define "slave#{i}" do |c|
+        c.vm.provider "virtualbox" do |v|
+          v.memory = 2048
+          v.cpus = 2
+        end
+        c.vm.network "private_network", ip: "192.168.2.#{i}"
+        c.vm.hostname = "slave#{i}.mesos.boom"
+#        c.vm.provision "ansible" do |a|
+#          a.limit = "all"
+#          a.playbook = "playbooks/mesos-single/playbook.yml"
+#          a.groups = {
+#            "mesos-slave" => ["slave#{i}"]
+#          }
+#          a.extra_vars = {
+#            domain_name: "slave#{i}.mesos.boom",
+#            host_ip: "192.168.2.#{i}",
+#            }
+#        end
+      end
+    end
+end
---
+++ plugins/filter_plugins/filters.py
@@ -0,0 +1,187 @@
+from ansible import errors
+from copy import deepcopy
+import json
+
+def to_group_vars(host_vars, groups, target = 'all'):
+    data = []
+    for host in groups[target]:
+        data.append(host_vars[host])
+    return data
+
+def not_in_list(list, haystack):
+    rv = []
+    for item in list:
+	if item not in haystack:
+	    rv.append(item)
+    return rv
+def add_suf(list, attr):
+    rv = []
+    for item in list:
+        rv.append(item + attr)
+    return rv
+def _map_on_attr(item, attr, default):
+    rv = item
+    attr_list = attr.split('.')
+    for a in attr_list:
+        if rv.has_key(a):
+            rv = rv[a]
+        else:
+            if default:
+                return default
+            else:
+                return False
+    return rv
+def filter_on_attr(list, attr, invert=False):
+    rv = []
+    for item in list:
+        r = _map_on_attr(item, attr, False);
+        if bool(r) ^ invert:
+            rv.append(item)
+    return rv
+def map_on_attr(list, attr, default=False):
+    rv = []
+    for item in list:
+        r = _map_on_attr(item, attr, default);
+        if r is not False:
+            rv.append(r)
+    return rv
+def dict_merge(a, b):
+    if not isinstance(b, dict):
+        return b
+    result = deepcopy(a)
+    for k, v in b.iteritems():
+        if k in result and isinstance(result[k], dict):
+                result[k] = dict_merge(result[k], v)
+        else:
+            result[k] = deepcopy(v)
+    return result
+def multiple_dict_merge(list_dicts):
+    result = {}
+    for obj in list_dicts:
+        if isinstance(obj, dict) and len(obj):
+            result = dict_merge(result, obj)
+    return result
+def syslog_facility_from_str(list):
+    rv = []
+    for item in list:
+        a = item.split(".");
+        rv.append(a[0])
+    return rv
+def syslog_severity_from_str(list, default=''):
+    rv = []
+    for item in list:
+        a = item.split(".");
+        if a[1]:
+            rv.append(a[1])
+        elif default:
+            rv.append(default)
+            
+    return rv
+def syslog_facility_to_prio(string):
+    fac_map = dict(
+        kern     =  0 ,  
+        user     =  1 ,
+        mail     =  2 ,
+        daemon   =  3 ,
+        auth     =  4 ,
+        syslog   =  5 ,
+        lpr      =  6 ,
+        news     =  7 ,
+        uucp     =  8 ,
+        cron     =  9 ,
+        security =  10,
+        ftp      =  11,
+        ntp      =  12,
+        logaudit =  13,
+        logalert =  14,
+        clock    =  15,
+        local0   =  16,
+        local1   =  17,
+        local2   =  18,
+        local3   =  19,
+        local4   =  20,
+        local5   =  21,
+        local6   =  22,
+        local7   =  23)
+    sev_map = dict(
+        emerg   = 0,
+        alert   = 1,
+        crit    = 2,
+        error   = 3,
+        warning = 4,
+        notice  = 5,
+        info    = 6,
+        debug   = 7) 
+    a = string.split(".");
+    if a[0] not in fac_map or a[1] not in sev_map:
+        raise errors.AnsibleFilterError('Bad "<facility>.<severity>" pair.')
+    return (fac_map[a[0]] * 8) + sev_map[a[1]]
+
+def multiple_dicts(custom_tmpls, counts):
+    res = []
+    for t in custom_tmpls:
+        tmpl = custom_tmpls[t]
+        for c in range(counts[t]):
+            o = {}
+            for p in tmpl:
+                o[p] = tmpl[p].replace("%COUNT%", str(c))
+            res.append(o)
+    return res
+
+def contain_substrs(list, substrs):
+    res = []
+    for source in list:
+        for substr in substrs:
+            if substr in source:
+                res.append(source)
+                break
+    return res
+
+def has_attr(list, attr):
+    result = []
+    attrs_val = attr.split("=")
+    attr_val =  None if len(attrs_val) < 2 else attrs_val[1]
+
+    for item in list:
+        o = _map_on_attr(item, attrs_val[0], False)
+        if o is not False:
+            if attr_val == None or attr_val == o:
+                result.append(item)
+    return result
+
+def is_empty(object):
+    return not len(a)
+
+def change_attr(src_dict, path, new_value):
+    result = src_dict
+    attr_list = path.split('.')
+    nesting = 0
+    rv = result
+    for a in attr_list:
+        if isinstance(rv, dict) and rv.has_key(a):
+            if nesting == (len(attr_list)-1):
+                rv[a] = new_value
+            else:
+                rv = rv[a]
+                nesting += 1
+        else:
+            return result
+    return result
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'add_suf': add_suf,
+            'map_on_attr': map_on_attr,
+            'syslog_facility_to_prio': syslog_facility_to_prio,
+            'to_group_vars': to_group_vars,
+            'dict_merge': dict_merge,
+            'not_in_list': not_in_list,
+            'multiple_dicts' : multiple_dicts,
+            'contain_substrs': contain_substrs,
+            'has_attr': has_attr,
+            'multiple_dict_merge': multiple_dict_merge,
+            'change_attr': change_attr,
+            'filter_on_attr': filter_on_attr
+        }
